d6ede0ae7dcf7052279e282e1074e21b21d31ff7
Jinan <130977025+Jinanging@users.noreply.github.com>
2025-03-05 12:56:58 +0900

    [Bronze V] Title: A+B - 3, Time: 192 ms, Memory: 17884 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,20 @@
+
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int number = scan.nextInt();
+		for(int i = 0 ; i < number; i++) {
+			int num1 = scan.nextInt();
+			int num2 = scan.nextInt();
+			System.out.println(num1 + num2);
+			
+		}
+		
+
+	}
+
+}
